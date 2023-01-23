3c91156b5a851e141eed499567683f8094570969
Vileni <vileni.jishkariani@gmail.com>
2023-01-23 10:29:14 +0400

    feat: add important piece

---
+++ src/util/guards/roles.guard.ts
@@ -0,0 +1,28 @@
+import { CanActivate, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: any): boolean {
+    const Handler = this.reflector.get<string[]>('roles', context.getHandler());
+    const Controller = this.reflector.get<string[]>(
+      'roles',
+      context.getClass(),
+    );
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    // check if the user has the required role
+    if (Controller?.includes(user.role)) {
+      return true;
+    }
+    if (Handler?.includes(user.role)) {
+      return true;
+    }
+
+    return false;
+  }
+}
