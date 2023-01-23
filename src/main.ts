b9a9e2cb46d764d379211f3b133988487c2b4c18
Vileni <vileni.jishkariani@gmail.com>
2023-01-23 10:30:00 +0400

    feat: add global validations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
-import { NestFactory } from '@nestjs/core';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('IQC API')
     .setDescription('IQC API Description')
