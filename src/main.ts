57c7c7f2429c39b47cdd78ebbd49f395206e4549
Vileni <vileni.jishkariani@gmail.com>
2023-01-23 05:37:07 +0400

    feat: add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('IQC API')
+    .setDescription('IQC API Description')
+    .setVersion('0.0.1')
+    .addBearerAuth(
+      {
+        description: 'JWT',
+        type: 'http',
+        name: 'Authorization',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        in: 'Header',
+      },
+      'Authorization',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
