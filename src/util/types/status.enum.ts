20c42bc525915413eeee4622fbeb779cef4153f0
Vileni <vileni.jishkariani@gmail.com>
2023-01-30 18:00:22 +0400

    add: ProjectStatus enum

--- src/util/types/status.enum.ts
+++ src/util/types/status.enum.ts
@@ -2,3 +2,8 @@ export enum Status {
   ACTIVE = 'ACTIVE',
   INACTIVE = 'INACTIVE',
 }
+export enum ProjectStatus {
+  DONE = 'DONE',
+  IN_PROGRESS = 'IN_PROGRESS',
+  NOT_STARTED = 'NOT_STARTED',
+}
