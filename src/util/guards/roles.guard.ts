29f1d5efd1f99b05439efb101f60231e1b75eed5
Vileni <vileni.jishkariani@gmail.com>
2023-01-30 18:03:27 +0400

    style: remove logs

--- src/util/guards/roles.guard.ts
+++ src/util/guards/roles.guard.ts
@@ -6,7 +6,6 @@ export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
 
   canActivate(context: any): boolean {
-    console.log('start validation1');
     const Handler = this.reflector.get<string[]>('roles', context.getHandler());
     const Controller = this.reflector.get<string[]>(
       'roles',
@@ -15,7 +14,6 @@ export class RolesGuard implements CanActivate {
 
     const request = context.switchToHttp().getRequest();
     const user = request.user;
-    console.log({ user });
 
     // check if the user has the required role
     if (Controller?.includes(user.role)) {
